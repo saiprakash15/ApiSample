5fd4fbb926a5e45300cd2098f269cca3ff4d8b4a
saiprakash15 <dsaip96@gmail.com>
2021-11-17 22:39:00 +0530

    JWT Token Flow added

--- ApiSample/Controllers/EmployeeController.cs
+++ ApiSample/Controllers/EmployeeController.cs
@@ -1,5 +1,6 @@
 ﻿using ApiSample.Models;
 using ApiSample.Repository;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,7 @@ namespace ApiSample.Controllers
 {
     //[Route("api/[controller]")]
     //[ApiController]
+    [Authorize]
     public class EmployeeController : Controller
     {
         private IEmployeeRepository _repo;
--- ApiSample/Startup.cs
+++ ApiSample/Startup.cs
@@ -1,4 +1,5 @@
 using ApiSample.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -7,9 +8,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ApiSample
@@ -27,16 +30,42 @@ namespace ApiSample
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            
+            var appsettingSection = Configuration.GetSection("Appsettings");
+            services.Configure<AppSettings>(Configuration.GetSection("Appsettings"));
             services.AddTransient<IEmployeeRepository,EmployeeRepository>();
             services.AddSwaggerGen();
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+            //JWT Configuration
+            var appSettings = appsettingSection.Get<AppSettings>();
+            var Key = Encoding.ASCII.GetBytes(appSettings.Key);
+            services.AddAuthentication(au => {
+                au.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                au.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(
+                jwt =>
+                {
+                    jwt.RequireHttpsMetadata = false;
+                    jwt.SaveToken = true;
+                    jwt.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+
+                    };
+
+                }
+                
+                );
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         { 
              app.UseSwagger();
-            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Employee API V1");
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Employee API V2");
              });
             if (env.IsDevelopment())
             {
